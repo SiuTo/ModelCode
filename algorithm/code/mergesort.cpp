d57eaf3af6ae6dabc82d0bfa074a21e8cbed7441
Xiaotao Liang <418xiaotao@163.com>
2016-12-05 15:02:07 +0800

    refactor: use template and iterator

--- algorithm/code/mergesort.cpp
+++ algorithm/code/mergesort.cpp
@@ -1,35 +1,29 @@
-#include <cstdio>
+#include <vector>
 
-const int Maxn=10000;
+using namespace std;
 
-int a[Maxn], b[Maxn];
+template<class Iterator>
+int mergesort(Iterator first, Iterator last) {
+    int n = last - first;
+    if (n < 2) return 0;
+    Iterator mid = first + n / 2;
+    int sum = mergesort(first, mid) + mergesort(mid, last);
 
-int mergesort(int l, int r)
-{
-	if (l==r) return 0;
-	int mid=(l+r)>>1;
-	int sum=mergesort(l, mid)+mergesort(mid+1, r);
-	int p=l, q=mid+1, h=l;
-	while (p<=mid && q<=r)
-		if (a[p]<=a[q]) b[h++]=a[p++]; else
-		{
-			b[h++]=a[q++];
-			sum+=mid-p+1;
-		}
-	while (p<=mid) b[h++]=a[p++];
-	while (q<=r) b[h++]=a[q++];
-	for (int i=l; i<=r; ++i) a[i]=b[i];
-	return sum;
+    typedef typename iterator_traits<Iterator>::value_type value_type;
+    vector<value_type> seq(n);
+    auto r = seq.begin();
+    Iterator p = first, q = mid;
+
+    while (p < mid && q < last)
+        if (*p <= *q) {
+            *r++ = *p++;
+        } else {
+            *r++ = *q++;
+            sum += mid - p;
+        }
+    while (p < mid) *r++ = *p++;
+    while (q < last) *r++ = *q++;
+    for (int i = 0; i < n; ++i)
+        *(first + i) = seq[i];
+    return sum;
 }
-
-int main()
-{
-	int n;
-	scanf("%d", &n);
-	for (int i=0; i<n; ++i) scanf("%d", a+i);
-	printf("%d\n", mergesort(0, n-1));
-	for (int i=0; i<n; ++i) printf("%d ", a[i]);
-	printf("\n");
-	return 0;
-}
-
